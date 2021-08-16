2ee09e817805d994dffad60dbac615d66b51b167
SidneiRepetcki <sid.r.junior@hotmail.com>
2021-08-15 22:56:01 -0300

    feat: color picker screen

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# colorPiker
-projeto Vuejs criando um componente ColorPiker
+# Component Color Picker 
+by: SidneiRepetcki 
+
+## Install all dependencies
+```
+npm install
+```
+
+### Run project for development
+```
+npm start
+```
+
+### Run project for development using Docker
+download docker: https://docs.docker.com/engine/install/
+```
+npm run serve
+```
+
+### Generate distribution file
+```
+npm run build
+```
+
+### Check code integrity
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <square :initialSelectedColor="initialColor" :colorPickerOptions="selectableColors" />
+  </div>
+</template>
+
+<script>
+import square from './components/Square.vue';
+
+export default {
+  name: 'App',
+  components: {
+    square,
+  },
+  data() {
+    return {
+      initialColor: {
+        name: 'PRETO',
+        color: '#000000',
+      },
+      selectableColors: [
+        {
+          name: 'ROXO',
+          color: '#6f56f6',
+        },
+        {
+          name: 'VERDE',
+          color: '#00e285',
+        },
+        {
+          name: 'AMARELO',
+          color: '#ffb858',
+        },
+        {
+          name: 'ROSA ESCURO',
+          color: '#ff1261',
+        },
+        {
+          name: 'ROSA CLARO',
+          color: '#ffa8c1',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Quicksand', sans-serif;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
